64147bbe739f3d1005748ca5ab40904049e768ce
sook <jisuk0415@naver.com>
2025-01-04 01:52:08 +0900

    250104 지숙(test.py - 관광지 데이터 DB 삽입)

--- test.py
+++ test.py
@@ -1,24 +1,21 @@
 import pandas as pd
-import mysql.connector
+import pymysql
+## pip install pymysql
 
-data = pd.read_csv("D:/dev/Plan4Land/Plan4Land_API/tour.csv")
+data_raw = pd.read_csv("tour.csv")
+data = data_raw.dropna(subset=['sigungu_code']) ## 시군구코드 없는애들 그냥 뺐어요
+df = data.fillna('')
 
-df = data.iloc[:5, :]
-df = df.fillna('')
-
-print("연결 시작")
 # MySQL 연결
-connection = mysql.connector.connect(
-    host='127.0.0.1',
+conn = pymysql.connect(
+    host='localhost',
     user='plan4plan',
     password='plan1234',
     database='plan_4_land_db',
-    port=3306
+    charset='utf8mb4' # 이모지와 다양한 유니코드 문자를 지원
 )
 
-print("접속 시작")
-
-cursor = connection.cursor()
+cursor = conn.cursor()
 
 # 데이터 삽입 쿼리
 insert_query = """
@@ -26,17 +23,13 @@ INSERT INTO Travel_Spot (spot_id, title, tel, thumbnail, area_code, sigungu_code
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
 """
 
-print("여기는 오니?")
-
 # 데이터프레임의 각 행을 데이터베이스에 삽입
 for index, row in df.iterrows():
     cursor.execute(insert_query, tuple(row))
 
 # 커밋하여 데이터베이스에 반영
-connection.commit()
+conn.commit()
 
 # 커서 및 연결 종료
 cursor.close()
-connection.close()
-
-print("데이터가 성공적으로 삽입되었습니다.")
+conn.close()
