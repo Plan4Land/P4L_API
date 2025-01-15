0f6829deb3f7cccd5b111a19f88f2c170110900b
BeigeSeason <kima606@gmail.com>
2025-01-15 17:47:28 +0900

    2025.01.15 강휘 회원 role 변경

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 #  pip install apscheduler
 import atexit
 
-from routes.payment import new_membership
+from routes.payment import new_membership, process_payments
 from routes.tour_api import get_tour
 from routes.holiday_api import get_holiday
 
@@ -18,6 +18,12 @@ app.add_url_rule("/api/holiday", "get_holiday",
 
 app.add_url_rule("/pay/new-membership", "insert-membership",
                  new_membership, methods=['POST'])
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(process_payments, 'cron', hour=9, minute=30)
+scheduler.start()
+
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == "__main__":
     app.run()
--- routes/payment.py
+++ routes/payment.py
@@ -5,16 +5,25 @@ import pymysql.cursors
 from flask import request, jsonify
 from datetime import datetime
 import pytz
+from contextlib import contextmanager
 
 from routes.payment_api import schedule_payment
 
-conn = pymysql.connect(
-    host='localhost',
-    user='plan4plan',
-    password='plan1234',
-    database='plan_4_land_db',
-    charset='utf8mb4'  # 이모지와 다양한 유니코드 문자를 지원
-)
+
+@contextmanager
+def get_db_connection():
+    conn = pymysql.connect(
+        host='localhost',
+        user='plan4plan',
+        password='plan1234',
+        database='plan_4_land_db',
+        charset='utf8mb4'  # 이모지와 다양한 유니코드 문자를 지원
+    )
+    try:
+        yield conn
+    finally:
+        conn.close()
+
 
 GET_MEMBER_UID = """
 SELECT member_id FROM member WHERE id=%s
@@ -38,7 +47,11 @@ ON MS.MEMBER_UID=I.MEMBER_ID
 WHERE DATE(PAYMENT_DATE) = %s AND ACTIVATE=1 AND CANCLE=0
 """
 
+IS_MEMBERSHIP = """
+UPDATE MEMBER SET ROLE=%s WHERE ID=%s
+"""
 
+# 날짜 형식 변경
 def convert_datetime(iso_string, to_timezone):
     utc = pytz.utc
     target_tz = pytz.timezone(to_timezone)
@@ -46,31 +59,32 @@ def convert_datetime(iso_string, to_timezone):
     dt_utc = dt.astimezone(utc)
     return dt_utc.astimezone(target_tz).strftime('%Y-%m-%d %H:%M:%S')
 
-
-def get_current_day(timezone='Asia/Seoul'):  # 오늘 날짜를 UTC로 가져오기
+# 오늘 날짜 가져오기
+def get_current_day(timezone='Asia/Seoul'):  
     now_utc = datetime.now(pytz.utc)  # 지정된 시간대로 변환
     target_timezone = pytz.timezone(timezone)
     now_target = now_utc.astimezone(target_timezone)
     return now_target.strftime('%Y-%m-%d')
 
-
+# 결제 예약 시 당일 2시로 예약 설정
 def get_time_to_pay():
     now = datetime.now(pytz.timezone('Asia/Seoul')).replace(hour=14, minute=0, second=0, microsecond=0)
     return now.isoformat()
 
-
+# 참조로 사용할 uid 획득
 def get_uid(user_id):
     try:
-        cursor = conn.cursor()
-        cursor.execute(GET_MEMBER_UID, (user_id,))
-        result = cursor.fetchone()
-        if result:
-            return result[0]
+        with get_db_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(GET_MEMBER_UID, (user_id,))
+                result = cursor.fetchone()
+                if result:
+                    return result[0]
     except Exception as e:
         print(e)
         return None
 
-
+# 새 멤버십 등록
 def new_membership():
     print('new membership')
     data = request.json
@@ -83,13 +97,16 @@ def new_membership():
     print(member_id)
 
     try:
-        with conn.cursor() as cursor:
-            cursor.execute(MEMBERSHIP_INSERT, (member_id, billing_key, expire_date, payment_date, pay_type))
-            membership_id = cursor.lastrowid
-            insert_record(cursor, data, membership_id)
-            conn.commit()
-            print('Membership inserted')
-        return jsonify({'message': 'User created successfully'}), 201
+        with get_db_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("START TRANSACTION")
+                cursor.execute(MEMBERSHIP_INSERT, (member_id, billing_key, expire_date, payment_date, pay_type))
+                change_role(cursor, member_id, True)
+                membership_id = cursor.lastrowid
+                insert_record(cursor, data, membership_id)
+                cursor.execute("COMMIT")
+                print('Membership inserted')
+            return jsonify({'message': 'User created successfully'}), 201
     except Exception as e:
         conn.rollback()
         print(e)
@@ -98,7 +115,7 @@ def new_membership():
     finally:
         conn.close()
 
-
+# 결제 시 기록 테이블에 삽입
 def insert_record(cursor, json_data, membership_pk):
     # data = request.json
     data = json_data
@@ -115,53 +132,64 @@ def insert_record(cursor, json_data, membership_pk):
         print(f'Record Insert Error : {e}')
         raise
 
+# 멤버십 결제/해지 시 유저 role 변경
+def change_role(cursor, user_id, keep_going):
+    try:
+        if keep_going:
+            cursor.execute(IS_MEMBERSHIP, ('ROLE_MEMBERSHIP', user_id,))
+        else:
+            cursor.execute(IS_MEMBERSHIP, ('ROLE_GENERAL', user_id,))
+    except Exception as e:
+        print(e)
+        raise
 
+# 정기결제 리스트 반환
 def get_payment_list():
     today = get_current_day()
     try:
-        with conn.cursor(pymysql.cursors.DictCursor) as cursor:
-            cursor.execute(PAY_LIST_QUERY, (today,))
-            result = cursor.fetchall()
-            return result
+        with get_db_connection() as conn:
+            with conn.cursor(pymysql.cursors.DictCursor) as cursor:
+                cursor.execute(PAY_LIST_QUERY, (today,))
+                result = cursor.fetchall()
+                return result
     except Exception as e:
         print(e)
         print('Payment List Rejected')
         return ''
 
-
+# 랜덤 결제건 ID
 def random_id():
     return binascii.hexlify(os.urandom(8)).decode()
 
-
+# 정기결제
 def process_payments():
     payment_data = get_payment_list()
 
     for data in payment_data:
         try:
-            with conn.cursor() as cursor:
-                cursor.execute('START TRANSACTION')
-                payment_id = random_id()
-                customer_id = data['ID']
-                billing_key = data['BILLING_KEY']
-                pay_type = data['PAY_TYPE']
-                membership_id = data['MEMBERSHIP_ID']
-                time_to_pay = get_time_to_pay()
-                pay_req_time = get_current_day()
+            with get_db_connection() as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute('START TRANSACTION')
+                    payment_id = random_id()
+                    customer_id = data['ID']
+                    billing_key = data['BILLING_KEY']
+                    pay_type = data['PAY_TYPE']
+                    membership_id = data['MEMBERSHIP_ID']
+                    time_to_pay = get_time_to_pay()
+                    pay_req_time = get_current_day()
 
+                    response = schedule_payment(payment_id, billing_key, customer_id, time_to_pay)
+                    insert_record_monthly(cursor, payment_id, pay_req_time, pay_type, customer_id, membership_id)
 
-                response = schedule_payment(payment_id, billing_key, customer_id, time_to_pay)
-                insert_record_monthly(cursor, payment_id, pay_req_time, pay_type, customer_id, membership_id)
-
-                cursor.execute('COMMIT')
-                print(f"Payment scheduled successfully: {response}")
+                    cursor.execute('COMMIT')
+                    print(f"Payment scheduled successfully: {response}")
         except Exception as e:
             cursor.execute('ROLLBACK')
             print(f"Failed to schedule payment: {e}")
 
-
+# 정기결제 기록
 def insert_record_monthly(cursor, record_id, pay_time, pay_type, member_id, membership_id):
     try:
-
         cursor.execute(RECORD_INSERT, (record_id, pay_time, pay_type, member_id, membership_id))
         print('Record inserted')
     except Exception as e:
