732f00250d8b65018e64879dc147a08e3a62c4ad
pjs2024 <jisuk0415@naver.com>
2025-02-06 16:31:21 +0900

    250206 지숙

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ data.loc[data['type_id'].isin([12, 14, 25, 28, 38]), 'type_id'] = 100
 data.loc[data['type_id'] == 32, 'type_id'] = 200
 data.loc[data['type_id'] == 39, 'type_id'] = 300
 data = data[data['type_id'] != 15]
+data['thumbnail'] = data['thumbnail'].str.replace("http://", "https://", regex=False)
 df = data.fillna('')
 
 # MySQL 연결
@@ -17,18 +18,18 @@ conn = pymysql.connect(
     user='plan4plan',
     password='plan1234',
     database='plan_4_land_db',
-    charset='utf8mb4' # 이모지와 다양한 유니코드 문자를 지원
+    charset='utf8mb4'
 )
 
 cursor = conn.cursor()
-delete_query = "DELETE FROM Travel_Spot"
+delete_query = "DELETE FROM travel_spot"
 
 cursor.execute(delete_query)
 conn.commit()
 
 # 데이터 삽입 쿼리
 insert_query = """
-INSERT INTO Travel_Spot (spot_id, title, tel, thumbnail, area_code, sigungu_code, addr1, addr2, cat1, cat2, cat3, type_id, created_time, modified_time, mapx, mapy)
+INSERT INTO travel_spot (spot_id, title, tel, thumbnail, area_code, sigungu_code, addr1, addr2, cat1, cat2, cat3, type_id, created_time, modified_time, mapx, mapy)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
 """
 
